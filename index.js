bd232e9bffe62b8b0320cb4a3c5349c918c61809
Milad Amiri <miladmarketing282.com>
2023-01-17 13:57:35 +0800

    added

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import express from "express";
 import cors from "cors";
 import productRouter from "./routes/Routes.js";
+import dotenv from "dotenv";
 
 const app = express();
-const port = process.env.PORT || 5000;
+dotenv.config();
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("This is home page")
+  res.send("This is home page");
 });
 
-// all products 
+// all products
 app.use("/api/products", productRouter);
 
-
 app.listen(port, () => {
-    console.log(`Port is running in port ${port}`);
-})
+  console.log(`Port is running in port ${port}`);
+});
